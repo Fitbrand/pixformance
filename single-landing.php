f2491a937be1e88a75c3950554db9373e12e6e34
frankspin89 <info@frankspin.nl>
2017-10-20 10:11:12 +0200

    Updated landing page

--- single-landing.php
+++ single-landing.php
@@ -18,77 +18,105 @@ get_header(); ?>
 <?php get_template_part( 'template-parts/page-header' ); ?>
 <div id="main">
   <div class="wrapper">
-    <div class="layout p9-t m9-b p3-lr p6-t-small">
+  <div class="stroke stroke-product-2 active"></div>
+    <div class="stroke stroke-product-3 active"></div>
+    
 
-    <?php if(get_field('landing_type') === 'blog') : ?>
+    <?php if(get_field('landing_type') === 'video') : ?>
+    
 
-    <div class="grid grid-1-3 grid-1-3-reversed  m6-b p3-lr ">
-							<div class="grid-item">
-								<?php the_title( '<h1 class="f2 m3-b">', '</h1>' );  ?>
-								<?php the_content(); ?>
-							</div>
-							<div class="grid-item p6-t">
-                <?php if(get_field('landing_pdf')) : ?>
-                <div class="m3-b">
-                  <strong class="block m1-b"><?php esc_html_e( 'Download insight as PDF', '_pixformance' ); ?></strong>
-                  <a href="<?php echo get_field('landing_pdf'); ?>" class="button button-secondary"><?php esc_html_e( 'Download PDF', '_pixformance' ); ?></a>
-                </div>
+    <?php if(get_field('landing_videos_boven')) : $i = 0; ?>
 
-                <?php endif; ?>
+        <?php while( have_rows('landing_videos_boven') ): the_row(); 
+          // vars
+          $image = get_sub_field('image');
+          $title = get_sub_field('title');
+          $description = get_sub_field('description');
+          $duration = get_sub_field('duration')
+          ?>
 
-								<div class="m1-b"><?php _pixformance_posted_on(); ?></div>
-					
-								<div class="m3-b">
-									
-									<strong><?php esc_html_e( 'Share on', '_pixformance' ); ?></strong>
-									<div class="blog-links" id="share">
-										<!-- Facebook -->
-										<a href="http://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>" target="_blank" class="no-underline">
-                    Facebook <span class="arrow-right">&nbsp;</a>
-                </a>
-                <!-- Twitter -->
-                <a href="https://twitter.com/share?url=<?php the_permalink(); ?>&amp;text=Pixformance&amp;hashtags=pixformance" target="_blank" class="no-underline">
-                  Twitter <span class="arrow-right">&nbsp;</a>
-                </a>
-                <!-- LinkedIn -->
-                <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?php the_permalink(); ?>" target="_blank" class="no-underline">
-                    Linkedin <span class="arrow-right">&nbsp;</a>
-                </a>
-									</div>
-								</div>
-								<p><a href="<?php echo esc_url( home_url( '/contact' ) ); ?>" class="button"><?php esc_html_e( 'Contact us', '_pixformance' ); ?></a></p>
-							</div>
-						</div>
-				<div>
+          <?php if($i === 0) : ?>
+          <div class="wrapper">
+            <div class="layout p3-t p3-lr p6-t-small">
+            <div class="grid grid-1-3 m9-b v-align-center p3-lr ">
+              <div class="grid-item"><img class="m3-b" style="max-height: 220px; <?php if($size): ?> max-width: <?php echo $size;?>px; <?php endif; ?>" src="<?php echo wp_get_attachment_image_src( $image, 'full' )[0] ?>" alt=""></div>
+              <div class="grid-item">
+                <h3 class="f3 m2-b"><?php echo $title; ?></h3>
+                <?php echo $content; ?>
+              </div>
+            </div>
 
-    <?php else : ?>
+            </div>
+          </div>
 
-      <div class="grid grid-halves grid-gap-large grid-image-right">
+          <?php else : ?>
+
+          <div class="wrapper bg-green m3-b">
+            <div class="layout p3-t p3-lr p6-t-small">
+            <div class="grid grid-3-1 grid-3-1-reversed m6-b v-align-center p3-lr">
+      <div class="grid-item"><img class="m3-b" style="max-height: 220px; <?php if($size): ?> max-width: <?php echo $size;?>px; <?php endif; ?>" src="<?php echo wp_get_attachment_image_src( $image, 'full' )[0] ?>" alt=""></div>
+      <div class="grid-item m3-b">
+        <h3 class="f3 m2-b"><?php echo $title; ?></h3>
+        <?php echo $content; ?>
+      </div>
+    </div>
+
+            </div>
+          </div>
+
+          <?php endif; ?>
+
+
+        <?php $i++; endwhile; ?>
+
+<?php endif; ?>
+
+
+<div class="layout p3-t p3-lr p6-t-small">
+    <div class="grid grid-halves grid-gap-large grid-image-right">
         <div class="grid-item">
-        <?php if(get_field('landing_image')) : ?>
-          <?php $image = get_field('landing_image'); ?>
-          <img class="m3-b" src="<?php echo wp_get_attachment_image_src( $image, 'full' )[0] ?>" />
-        <?php endif; ?>
+       
         <?php if(get_field('landing_videos')) : ?>
-
-        <h3 class="f3 m3-b"><?php esc_html_e( 'Watch more video\'s', '_pixformance' ); ?></h3>
 
         <?php while( have_rows('landing_videos') ): the_row(); 
           // vars
           $image = get_sub_field('image');
           $title = get_sub_field('title');
           $description = get_sub_field('description');
+          $duration = get_sub_field('duration')
           ?>
 
           <div class="m3-b">
             <img class="m1-b" src="<?php echo wp_get_attachment_image_src( $image, 'full' )[0] ?>" />
-            <h4 class="f4"><?php echo $title; ?></h4>
-            <?php echo $description; ?>
+            <div class="grid grid-halves grid-gap-large">
+              <div class="grid-item ttu text-right">
+                <?php esc_html_e( 'Duration:', '_pixformance' ); ?> <?php echo $duration; ?>
+              </div>
+              <div class="grid-item">
+                <h4 class="f4 m0"><?php echo $title; ?></h4>
+                <?php echo $description; ?>
+              </div>
+            </div>
           </div>
-
-
         <?php endwhile; ?>
 
+        <?php endif; ?>
+        </div>
+        <div class="grid-item">
+          <?php the_content(); ?>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+
+    <?php else : ?>
+    <div class="layout p3-t p3-lr p6-t-small">
+      <div class="grid grid-halves grid-gap-large grid-image-right">
+        <div class="grid-item">
+        <?php if(get_field('landing_image')) : ?>
+          <?php $image = get_field('landing_image'); ?>
+          <img class="m3-b" src="<?php echo wp_get_attachment_image_src( $image, 'full' )[0] ?>" />
         <?php endif; ?>
         </div>
         <div class="grid-item">
@@ -99,10 +127,30 @@ get_header(); ?>
   </div>
 
   <?php endif; ?>
+  <?php if(get_field('landing_form_title')) : ?>
+  <div class="wrapper">
+    <div class="layout p3-t m1-b p3-lr text-center">
+      <?php the_field('landing_form_title'); ?>
+    </div>
+  </div>
+  <?php endif; ?>
+
+
+
+  <?php if(get_field('landing_pdf')) : ?>
+  <div class="wrapper bg-green clearfix">
+    <div class="layout p3-t m3-b p3-lr white">
+      <div class="w-medium center">
+        <p class="text-center"><a href="<?php the_field('landing_pdf'); ?>" class="button button-secondary ttu"><?php esc_html_e( 'Download Nu', '_pixformance' ); ?></a></p>
+      </div>
+    </div>
+  </div>
+  <?php endif; ?>
+
   <?php if(get_field('landing_form')) : ?>
   <div class="wrapper bg-green clearfix">
-    <div class="layout p9-t m9-b p3-lr white">
-      <div class="w-half">
+    <div class="layout p3-t m3-b p3-lr white">
+      <div class="w-medium center">
         <?php echo do_shortcode(get_field('landing_form')); ?>
       </div>
     </div>
--- template-parts/naked-header.php
+++ template-parts/naked-header.php
@@ -1,6 +1,14 @@
-<header id="masthead" class="site-header flex-spaced p3-lr p1-tb">
-  <div class="site-branding center">
-    <a class="block" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+<header id="masthead" class="site-header flex-spaced p3-lr p1-tb p0-tb">
+
+
+<!-- Later in the code, use it... -->
+<div class="site-branding center">
+  <div class="fitnes-insights">
+    <svg class="icon-insights">
+      <use xlink:href="#icon-insights" xmlns:xlink="http://www.w3.org/1999/xlink"></use>
+    </svg>
+  </div>
+    <a class="block text-center logo-insights" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
       <svg class="logo">
         <use xlink:href="#logo" xmlns:xlink="http://www.w3.org/1999/xlink"></use>
       </svg>
